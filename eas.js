035f613a4fbaf56501c8394a74d2e40438dd2e8a
Scott <scappworks@gmail.com>
2020-08-29 06:32:40 -0400

    updated 'eas.js' to get the correct number of cells with the given numbers

--- eas.js
+++ eas.js
@@ -8,18 +8,16 @@ let populateGrid = () => {
 
     for (i = 1; i <= gridRowNumber; i++) {
         for (j = 1; j <= gridColumnNumber; j++) {
-            let newChild = gridChild[0].cloneNode();
-            newChild.style.setProperty("grid-column-start", j);
-            newChild.style.setProperty("grid-column-end", j);
-            newChild.style.setProperty("grid-row-start", i);
-            newChild.style.setProperty("grid-row-end", i);
-            gridParent.appendChild(newChild);
+            if (i !== 1 || j !== 1) {
+                let newChild = gridChild[0].cloneNode();
+                newChild.style.setProperty("grid-column-start", j);
+                newChild.style.setProperty("grid-column-end", j);
+                newChild.style.setProperty("grid-row-start", i);
+                newChild.style.setProperty("grid-row-end", i);
+                gridParent.appendChild(newChild);
+            }
         }
     }
-
-    console.log(gridParent.childElementCount);
 };
-
-console.log(gridParent.length);
 
 populateGrid();
