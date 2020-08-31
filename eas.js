b9f30353a99ef5364e759ccd59ceb0e062b53432
Scott <scappworks@gmail.com>
2020-08-31 08:06:34 -0400

    figuring out how to make colors of individual boxes 10% darker every time the counter increments on each its  box. all work in 'eas.js' within the 'addEventListener' for adding new children to parent.

--- eas.js
+++ eas.js
@@ -63,6 +63,18 @@ let populateGrid = (col = -1, row = -1, started = false) => {
             newChild.style.setProperty("grid-row-end", i);
             newChild.addEventListener("mouseover", function() {
                 let color = getRandomColor();
+
+                if ((getTimesTouched(
+                    newChild.style.getPropertyValue("grid-row-start"),
+                    newChild.style.getPropertyValue("grid-column-start"),
+                    gridColumnNumber)) 
+                    > 0) {
+                        /****************************IT'S HERE******************/
+                        newColor = (color & 0xfefefe) >> 0.1;
+                        color += newColor;
+                        }
+                    
+
                 newChild.style.setProperty("background", color);
 
                 if (timesTouchedNumberArray.length > 0) {
